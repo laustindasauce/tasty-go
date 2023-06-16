5ef779196fe7494bda1fb12db64943810a38196e
Austin Spencer <abspencer2097@gmail.com>
2023-06-15 19:44:06 -0700

    fix session for remember-token reauth

--- session.go
+++ session.go
@@ -2,57 +2,40 @@ package tasty
 
 import (
 	"net/http"
+
+	"github.com/austinbspencer/tasty-go/models"
 )
 
-type LoginInfo struct {
-	Login         string `json:"login"`
-	Password      string `json:"password"`
-	TwoFactorCode *string
-	RememberMe    bool
-}
-
-type User struct {
-	Email      string `json:"email"`
-	Username   string `json:"username"`
-	ExternalID string `json:"external-id"`
-}
-
-type Session struct {
-	User          User    `json:"user"`
-	SessionToken  *string `json:"session-token"`
-	RememberToken *string `json:"remember-token"`
-}
-
 type sessionResponse struct {
-	Session Session `json:"data"`
-	Context string  `json:"context"`
+	Session models.Session `json:"data"`
 }
 
 // Create a new user session.
-func (c *Client) CreateSession(login LoginInfo) (Session, *Error) {
+func (c *Client) CreateSession(login models.LoginInfo, twoFactorCode *string) (models.Session, *Error) {
 	path := "/sessions"
 
 	session := new(sessionResponse)
 
 	header := http.Header{}
-	if login.TwoFactorCode != nil {
-		header.Add("X-Tastyworks-OTP", *login.TwoFactorCode)
+
+	if twoFactorCode != nil {
+		header.Add("X-Tastyworks-OTP", *twoFactorCode)
 	}
 
 	err := c.request(http.MethodPost, path, header, nil, login, session)
 	if err == nil {
 		c.Session = session.Session
 	} else {
-		return Session{}, err
+		return models.Session{}, err
 	}
 
 	return session.Session, nil
 }
 
 // Validate the user session.
-func (c *Client) ValidateSession() (Session, *Error) {
+func (c *Client) ValidateSession() (models.Session, *Error) {
 	if c.Session.SessionToken == nil {
-		return Session{}, &Error{Message: "Session is invalid: Session Token cannot be nil."}
+		return models.Session{}, &Error{Message: "Session is invalid: Session Token cannot be nil."}
 	}
 	path := "/sessions/validate"
 
@@ -62,11 +45,11 @@ func (c *Client) ValidateSession() (Session, *Error) {
 	header.Add("Authorization", *c.Session.SessionToken)
 
 	err := c.request(http.MethodPost, path, header, nil, nil, session)
-	if err == nil {
-		c.Session = session.Session
-	} else {
-		return Session{}, err
+	if err != nil {
+		return models.Session{}, err
 	}
+
+	c.Session = session.Session
 
 	return session.Session, nil
 }
